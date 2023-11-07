3c2555e4f1fe3da085d40f2f21b042859b25ab65
kawarien <ibrahimabbaadam@yahoo.fr>
2023-11-07 14:53:33 +0100

    second commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GraientContainer extends StatelessWidget {
+  const GraientContainer(this.color1, this.color2, {super.key});
+  const GraientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [color1, color2], begin: startAlignment, end: endAlignment),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
